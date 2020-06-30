53380ca72e227286e5d09565bec9d16324a80ce4
SashaEm <sashaem2002@gmail.com>
2020-07-01 00:44:08 +0300

    Enemies now behave like intended

---
+++ CAT-Game2/Assets/Scripts/EnemiesRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesRotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSens = 1f;
+
+    private Transform pillar;
+    private Vector3 mouseStart;
+    
+    void Awake()
+    {
+        pillar = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseStart = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {   
+            var rotation = (Input.mousePosition - mouseStart)* rotationSens;
+            pillar.Rotate(0f, 0f, -rotation.x);
+            mouseStart = Input.mousePosition;
+        }
+
+        
+    }
+}
